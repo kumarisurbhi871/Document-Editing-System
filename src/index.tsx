a67bf723cfb8edac244535230604ec451449fcc8
kumari surbhi <92303695+kumarisurbhi871@users.noreply.github.com>
2024-01-24 19:57:00 +0530

    Add files via upload

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {registerLicense} from '@syncfusion/ej2-base';
+registerLicense("Ngo9BigBOggjHTQxAR8/V1NAaF5cWWJCf0x1RXxbf1x0ZFRMYVtbRH9PIiBoS35RckViW3xfc3VSRmlVU0Z0")
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
